b3128913e4f6cc9f0d8a0e75e3a20cf16c0b87ef
finless88 <nazmul96@gmail.com>
2023-05-03 22:06:25 +0600

    reload page bug is fixed with spinner

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -16,14 +16,18 @@ const auth = getAuth(app);
 
 const AuthProviders = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const signIn = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   const logout = () => {
+    setLoading(true);
     return signOut(auth);
   };
 
@@ -33,6 +37,7 @@ const AuthProviders = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
       console.log("logged in user inside the authstate observer", loggedUser);
       setUser(loggedUser);
+      setLoading(false);
     });
 
     return () => {
@@ -46,6 +51,7 @@ const AuthProviders = ({ children }) => {
     signIn,
     updateProfile,
     logout,
+    loading,
   };
   return (
     <AuthContext.Provider value={authInformation}>
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,13 +2,18 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 const PrivateRoute = ({ children }) => {
   // to apply private route we have to check if user is available or not
   // to do it observe user state change in authContext
 
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
-  const from = location?.state?.from?.pathName || "/";
+  const from = location.state?.from?.pathName || "/";
+
+  if (loading) {
+    return <Spinner animation="border" variant="primary" />;
+  }
   if (user) {
     return children;
   }
